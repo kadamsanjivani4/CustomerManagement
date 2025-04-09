97e304f62a95a0e1848d4781b03e763c1042a024
kadamsanjivani4 <74709425+kadamsanjivani4@users.noreply.github.com>
2025-04-09 10:05:26 +0530

    Added a function

--- CustomerManagement/Controllers/CMSController.cs
+++ CustomerManagement/Controllers/CMSController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace CustomerManagement.Controllers
 {
@@ -16,11 +17,32 @@ namespace CustomerManagement.Controllers
         #region "Public Methods"
         [Route("display")]
         [HttpGet]
-        public async Task<ObjectResult> GetAllCustomers()
+        public async Task<ObjectResult> GetAllCustomers(int FTId,int objType)
         {
             try
             {
                 List<CLDisplayModel> customlabels = new List<CLDisplayModel>();
+                if (FTId == 3 && objType > 0)
+                {
+                    customlabels = await _appClass.GetCustomLabelsByObjTypeObjPointerAsync(FTId, objType, objPointer);
+                }
+                else
+                {
+                    customlabels = await _appClass.GetTableDisplayDataAsync(LanguageId, FTId);
+                }
+
+                return _response.OK(customlabels);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        public async Task<ObjectResult> GetCustomersById(int FTId, int objType)
+        {
+            try
+            {
+                CLDisplayModel customlabels = new CLDisplayModel();
                 if (FTId == 3 && objType > 0)
                 {
                     customlabels = await _appClass.GetCustomLabelsByObjTypeObjPointerAsync(FTId, objType, objPointer);
